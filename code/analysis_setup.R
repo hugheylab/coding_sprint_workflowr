9c57836415ef7a639684db2d571a1b3e18a8d676
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2022-01-20 15:16:21 -0600

    Fix analysis setup steps

--- code/analysis_setup.R
+++ code/analysis_setup.R
@@ -1,9 +1,13 @@
-descFile = 'DESCRIPTION'
-if (!file.exists(descFile)) usethis::use_description(fields = list(Imports = 'withr'), check_name = FALSE)
+# Edit below list of people in the lab to add analysis files and links!
 labNames = c('Dora', 'Elliot', 'Jake', 'Josh', 'Layla', 'Sample')
+indexRmd = readLines('analysis/index.Rmd')
 for (name in labNames) {
-  analysisFilename = file.path('analysis', paste0(name, '_file.Rmd'))
+  analysisFilename = file.path('analysis', paste0(name, '_analysis.Rmd'))
   if (!file.exists(analysisFilename)) {
     wflow_open(analysisFilename, change_wd = FALSE, edit_in_rstudio = FALSE)
   }
+  if (name != 'Sample') {
+    indexRmd = c(indexRmd, paste0(' * [', name, '](', name, '_analysis.html)'))
+  }
 }
+writeLines(indexRmd, 'analysis/index.Rmd')
