b6042c23013fd773c299f3c550c5eb8ac67f6f59
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2022-01-20 15:37:15 -0600

    Add comments to script.

--- code/analysis_setup.R
+++ code/analysis_setup.R
@@ -1,11 +1,14 @@
 # Edit below list of people in the lab to add analysis files and links!
 labNames = c('Dora', 'Elliot', 'Jake', 'Josh', 'Layla', 'Sample')
+# Reads index.Rmd to append links to
 indexRmd = readLines('analysis/index.Rmd')
 for (name in labNames) {
   analysisFilename = file.path('analysis', paste0(name, '_analysis.Rmd'))
+  # Creates analysis file if not exists.
   if (!file.exists(analysisFilename)) {
     wflow_open(analysisFilename, change_wd = FALSE, edit_in_rstudio = FALSE)
   }
+  # Appends to index.Rmd if not the sample
   if (name != 'Sample') {
     indexRmd = c(indexRmd, paste0(' * [', name, '](', name, '_analysis.html)'))
   }
