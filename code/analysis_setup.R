3dfa3ece5050a4e9675c68aff4c0bc4dddb5aafc
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2022-01-21 08:47:23 -0600

    Use lowercase for consistency

--- code/analysis_setup.R
+++ code/analysis_setup.R
@@ -1,5 +1,5 @@
 # Edit below list of people in the lab to add analysis files and links!
-labNames = c('Dora', 'Elliot', 'Jake', 'Josh', 'Layla', 'Sample')
+labNames = c('dora', 'elliot', 'jake', 'josh', 'layla', 'sample')
 # Reads index.Rmd to append links to
 indexRmd = readLines('analysis/index.Rmd')
 for (name in labNames) {
@@ -9,7 +9,7 @@ for (name in labNames) {
     wflow_open(analysisFilename, change_wd = FALSE, edit_in_rstudio = FALSE)
   }
   # Appends to index.Rmd if not the sample
-  if (name != 'Sample') {
+  if (name != 'sample') {
     indexRmd = c(indexRmd, paste0(' * [', name, '](', name, '_analysis.html)'))
   }
 }
