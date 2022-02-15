37fc02388038498df8d07033f28849c4b7c65d3e
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2022-02-15 10:14:34 -0600

    Use file.path, fix line breaks, use sprintf instead of paste0

--- code/analysis_setup.R
+++ code/analysis_setup.R
@@ -2,16 +2,14 @@ library('workflowr')
 # Edit below list of people in the lab to add analysis files and links!
 persons = c('dora', 'elliot', 'jake', 'josh', 'layla', 'sample')
 # Reads index.Rmd to append links to
-indexRmd = readLines('analysis/index.Rmd')
+indexRmd = readLines(file.path('analysis', 'index.Rmd'))
 for (person in persons) {
   analysisFilename = file.path('analysis', paste0(person, '_analysis.Rmd'))
   # Creates analysis file if not exists.
   if (!file.exists(analysisFilename)) {
-    wflow_open(analysisFilename, change_wd = FALSE, edit_in_rstudio = FALSE)
-  }
+    wflow_open(analysisFilename, change_wd = FALSE, edit_in_rstudio = FALSE)}
   # Appends to index.Rmd if not the sample
   if (person != 'sample') {
-    indexRmd = c(indexRmd, paste0(' * [', person, '](', person, '_analysis.html)'))
-  }
-}
+    indexRmd = c(indexRmd,
+                 sprintf(' * [%s](%s_analysis.html)', person, person))}}
 writeLines(indexRmd, 'analysis/index.Rmd')
